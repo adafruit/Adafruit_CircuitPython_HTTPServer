ebb7ca74fd57c7d969367ce02e58f3c51ae8cd35
michalpokusa <72110769+michalpokusa@users.noreply.github.com>
2023-07-13 08:54:39 +0000

    Added example for SSEResponse

---
+++ examples/httpserver_sse.py
@@ -0,0 +1,62 @@
+# SPDX-FileCopyrightText: 2023 Dan Halbert for Adafruit Industries
+#
+# SPDX-License-Identifier: Unlicense
+
+from time import monotonic
+import microcontroller
+import socketpool
+import wifi
+
+from adafruit_httpserver import Server, Request, Response, SSEResponse, GET
+
+
+pool = socketpool.SocketPool(wifi.radio)
+server = Server(pool, debug=True)
+
+
+sse_response: SSEResponse = None
+next_event_time = monotonic()
+
+HTML_TEMPLATE = """
+<html lang="en">
+    <head>
+        <title>Server-Sent Events Client</title>
+    </head>
+    <body>
+        <script>
+            const eventSource = new EventSource('/connect-client');
+
+            eventSource.onmessage = event => console.log('Event data:', event.data);
+            eventSource.onerror = error => console.error('SSE error:', error);
+        </script>
+    </body>
+</html>
+"""
+
+
+@server.route("/client", GET)
+def client(request: Request):
+    return Response(request, HTML_TEMPLATE, content_type="text/html")
+
+
+@server.route("/connect-client", GET)
+def connect_client(request: Request):
+    global sse_response
+
+    if sse_response is not None:
+        sse_response.close()  # Close any existing connection
+
+    sse_response = SSEResponse(request)
+
+    return sse_response
+
+
+server.start(str(wifi.radio.ipv4_address))
+while True:
+    server.poll()
+
+    # Send an event every second
+    if sse_response is not None and next_event_time < monotonic():
+        cpu_temp = round(microcontroller.cpu.temperature, 2)
+        sse_response.send_event(f"CPU: {cpu_temp}°C")
+        next_event_time = monotonic() + 1
