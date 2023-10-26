0ad3c2cffe07a5bf7e3a46dc356c774ba4555eb1
michalpokusa <72110769+michalpokusa@users.noreply.github.com>
2023-10-26 09:02:13 +0000

    Added example for using adafruit_templateengine with adafruit_httpserver

---
+++ examples/httpserver_templates.py
@@ -0,0 +1,61 @@
+# SPDX-FileCopyrightText: 2023 Michal Pokusa
+#
+# SPDX-License-Identifier: Unlicense
+import os
+import re
+
+import socketpool
+import wifi
+
+from adafruit_httpserver import Server, Request, Response, FileResponse
+
+try:
+    from adafruit_templateengine import render_template
+except ImportError as e:
+    raise ImportError("This example requires adafruit_templateengine library.") from e
+
+
+pool = socketpool.SocketPool(wifi.radio)
+server = Server(pool, "/static", debug=True)
+
+# Create /static directory if it doesn't exist
+try:
+    os.listdir("/static")
+except OSError as e:
+    raise OSError("Please create a /static directory on the CIRCUITPY drive.") from e
+
+
+@server.route("/")
+def directory_listing(request: Request):
+    path = request.query_params.get("path") or ""
+
+    # Remove .. and . from path
+    path = re.sub(r"\/(\.\.|\.)\/|\/(\.\.|\.)|(\.\.|\.)\/", "/", path).strip("/")
+
+    if path:
+        is_file = (
+            os.stat(f"/static/{path}")[0] & 0b_11110000_00000000
+        ) == 0b_10000000_00000000
+    else:
+        is_file = False
+
+    # If path is a file, return it as a file response
+    if is_file:
+        return FileResponse(request, path)
+
+    # Otherwise, return a directory listing
+    return Response(
+        request,
+        render_template(
+            "directory_listing.tpl.html",
+            context={
+                "path": path,
+                "items": os.listdir(f"/static/{path}"),
+            },
+        ),
+        content_type="text/html",
+    )
+
+
+# Start the server.
+server.serve_forever(str(wifi.radio.ipv4_address))
