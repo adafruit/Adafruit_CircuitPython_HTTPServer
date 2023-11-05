6a5d1425f9f943091bfcd61135c9782177b96fae
michalpokusa <72110769+michalpokusa@users.noreply.github.com>
2023-11-05 12:04:57 +0000

    Refactor and added trailing / for directories

--- examples/httpserver_templates.py
+++ examples/httpserver_templates.py
@@ -25,6 +25,10 @@ except OSError as e:
     raise OSError("Please create a /static directory on the CIRCUITPY drive.") from e
 
 
+def is_file(path: str):
+    return (os.stat(path.rstrip("/"))[0] & 0b_11110000_00000000) == 0b_10000000_00000000
+
+
 @server.route("/")
 def directory_listing(request: Request):
     path = request.query_params.get("path", "").replace("%20", " ")
@@ -32,26 +36,24 @@ def directory_listing(request: Request):
     # Preventing path traversal by removing all ../ from path
     path = re.sub(r"\/(\.\.)\/|\/(\.\.)|(\.\.)\/", "/", path).strip("/")
 
-    if path:
-        is_file = (
-            os.stat(f"/static/{path}")[0] & 0b_11110000_00000000
-        ) == 0b_10000000_00000000
-    else:
-        is_file = False
-
     # If path is a file, return it as a file response
-    if is_file:
+    if is_file(f"/static/{path}"):
         return FileResponse(request, path)
+
+    items = sorted(
+        [
+            item + ("" if is_file(f"/static/{path}/{item}") else "/")
+            for item in os.listdir(f"/static/{path}")
+        ],
+        key=lambda item: not item.endswith("/"),
+    )
 
     # Otherwise, return a directory listing
     return Response(
         request,
         render_template(
             "directory_listing.tpl.html",
-            context={
-                "path": path,
-                "items": os.listdir(f"/static/{path}"),
-            },
+            context={"path": path, "items": items},
         ),
         content_type="text/html",
     )
